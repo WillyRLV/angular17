b4a84f305a061ae697026d3d1a149ea43f743d40
William <Williamlv1998@gmail.com>
2024-05-10 11:54:18 -0500

    init:aprendiendo lo básico en angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserComponent],
   templateUrl: './app.component.html',
+  // template: `<h1>Hola mundo desde {{title.toLocaleLowerCase()}} </h1>
+  
+  // <app-user></app-user>
+  
+  // `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
